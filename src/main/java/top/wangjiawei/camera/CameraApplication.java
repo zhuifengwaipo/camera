f2584d5e4e7d0f78c3cb060fb13c14a436c03a80
zhuifengwaipo <934303587@qq.com>
2020-02-29 17:13:24 +0800

    add提交

---
+++ src/main/java/top/wangjiawei/camera/CameraApplication.java
@@ -0,0 +1,13 @@
+package top.wangjiawei.camera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CameraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CameraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/wangjiawei/camera/CameraApplicationTests.java
@@ -0,0 +1,12 @@
+package top.wangjiawei.camera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CameraApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
